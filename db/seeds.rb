e1e8e96deaf0a0d8e0a32056667970b309a039d9
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-04 14:02:16 +0500

    file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+ 
 if Doorkeeper::Application.count.zero?
     Doorkeeper::Application.create(name: 'Web Client', redirect_uri: '', scopes: '')
     Doorkeeper::Application.create(name: 'iOS Client', redirect_uri: '', scopes: '')
